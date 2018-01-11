9e9ccc978fc88366b52a2def1d5ae57ea5c13c18
dimitris.flores@mullenmail.com <Dimitris.Flores@MU-C1-10.mullen.pvt.k12.co.us>
2018-01-11 14:22:51 -0700

    initial commit

---
+++ Inheretance Quizzer/src/food.java
@@ -0,0 +1,22 @@
+
+public class food
+	{
+		private int number;
+		public food(int x)
+			{
+			number = x;
+			}
+		public int grab()
+			{
+			return get();
+			}
+		public void set(int y)
+			{
+			number = y;
+			}
+		public int get()
+			{
+			return number;
+			}	
+	}
+
---
+++ Inheretance Quizzer/src/sweets.java
@@ -0,0 +1,22 @@
+
+public class sweets extends food
+	{
+		private int numberTwo;
+		public sweets(int x)
+			{
+			super(x);
+			numberTwo = x;
+			}
+		public int grab()
+			{
+			return super.grab();
+			}
+		public void set(int y)
+			{
+			numberTwo = y;
+			}
+		public int get()
+			{
+			return numberTwo;
+			}
+	}
